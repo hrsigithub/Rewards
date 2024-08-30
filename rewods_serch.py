a3e5a1f29579e4f0ffa49886a7cddeec1990398c
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-08-31 08:10:36 +0900

    日本語対応予定、未完成

--- rewods_serch.py
+++ rewods_serch.py
@@ -5,22 +5,25 @@ import string
 
 pyautogui.FAILSAFE = True
 
+# 日本語の文字リストを定義（ひらがな、カタカナ、漢字の一部を含む）
+japanese_chars = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン一二三四五六七八九十'
+
+
 # 少し待機してからSpotlightを開く
 time.sleep(2)
 
 # Cmd + SpaceでSpotlightを開く
 pyautogui.keyDown('command')
 pyautogui.press('space')
-
-
 pyautogui.keyUp('command')
-
 
 # 少し待機
 time.sleep(1)
 
 # "Chrome"と入力
 pyautogui.typewrite('Chrome')
+
+
 
 # Enterキーを押してChromeを起動
 pyautogui.press('enter')
@@ -29,15 +32,19 @@ pyautogui.press('enter')
 time.sleep(3)
 
 # 10回の検索を実行
-for _ in range(90):
+for _ in range(5):
+
     # アドレスバーにフォーカスする
     pyautogui.hotkey('command', 'l')
 
-    # Googleのページが開くまで待機
+    # ページが開くまで待機
     time.sleep(3)
 
     # ランダムな検索文字列を生成
     search_text = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
+     # ランダムな日本語の検索文字列を生成
+    # search_text = ''.join(random.choices(japanese_chars, k=10))
 
     # 検索ワードを入力
     pyautogui.typewrite(search_text)
